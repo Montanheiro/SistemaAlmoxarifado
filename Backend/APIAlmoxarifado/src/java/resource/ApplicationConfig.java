af1067b008e6befa5a62ad53483e290d239d5359
Dione Alvino <dionegtba@gmail.com>
2017-04-30 17:08:17 -0300

    Criar ContatoResource e alterar AlunoResource

--- Backend/APIAlmoxarifado/src/java/resource/ApplicationConfig.java
+++ Backend/APIAlmoxarifado/src/java/resource/ApplicationConfig.java
@@ -31,6 +31,7 @@ public class ApplicationConfig extends Application {
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(resource.AdminResource.class);
         resources.add(resource.AlunoResource.class);
+        resources.add(resource.ContatoResource.class);
     }
     
 }
